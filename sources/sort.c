d068e3594407fed57b5e754cdc633243eec6f37d
ghambrec <ghambrec@student.42heilbronn.de>
2024-11-29 13:09:13 +0100

    created function move_to_top

--- sources/sort.c
+++ sources/sort.c
@@ -6,7 +6,7 @@
 /*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 15:03:15 by ghambrec          #+#    #+#             */
-/*   Updated: 2024/11/29 12:24:29 by ghambrec         ###   ########.fr       */
+/*   Updated: 2024/11/29 13:08:57 by ghambrec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,7 +94,6 @@ int	above_median(t_stack *stack)
 	return (FALSE);
 }
 
-
 void	calc_push_cost(t_stack *from, t_stack *to)
 {
 	int	cost_a;
@@ -149,11 +148,45 @@ t_stack *get_cheapest_stack(t_stack *stack)
 	return (cheapest);
 }
 
+// function to move both nodes at the top of their stacks
+void	move_to_top(t_stack *node1, t_stack *node2, t_stack **stack1, t_stack **stack2)
+{
+	if (above_median(node1) && above_median(node2))
+	{
+		while (node1 != *stack1 && node2 != *stack2)
+			rotate_both(stack1, stack2);
+	}
+	if (!above_median(node1) && !above_median(node2))
+	{
+		while (node1 != *stack1 && node2 != *stack2)
+			reverse_rotate_both(stack1, stack2);
+	}
+	while (node1 != *stack1)
+	{
+		if (above_median(node1))
+			rotate(stack1);
+		else
+			reverse_rotate(stack1);
+	}
+	while (node2 != *stack2)
+	{
+		if (above_median(node2))
+			rotate(stack2);
+		else
+			reverse_rotate(stack2);
+	}
+}
+
 void	push_to_b(t_stack **a, t_stack **b)
 {
 	t_stack *cheapest;
 
 	cheapest = get_cheapest_stack(*a);
+	if (cheapest != *a || cheapest->target != *b)
+	{
+		move_to_top(cheapest, cheapest->target, a, b);
+	}
+	
 }
 
 void	sort(t_stack **a, t_stack **b)
@@ -175,8 +208,8 @@ void	sort(t_stack **a, t_stack **b)
 	{
 		a_size--;
 		push(a, b);
-		// push(a, b); // for testing
 	}
+	push(a, b); // for testing
 	init_stack(*a, *b);
 	push_to_b(a, b);
 
